8047a126d52a0544a2163441c4546dde27fc7643
Wayne <zweijie1210@outlook.com>
2020-10-15 13:06:17 -0700

    finsh login, insert

--- src/com/weijiez/dao/UserDao_Imp.java
+++ src/com/weijiez/dao/UserDao_Imp.java
@@ -1,15 +1,49 @@
 package com.weijiez.dao;
 
+import com.weijiez.jdbc.JDBCUtils;
 import com.weijiez.user.User;
 
+import java.sql.*;
+
 public class UserDao_Imp implements UserDao{
+    private final String SQL_USER_LOGIN = "SELECT type FROM user WHERE username = ? AND password = ?";
+    private final String SQL_USER_INSERT = "INSERT INTO user VALUES(id, ?, ?, ?)";
+
     @Override
     public int login(User user) {
-        return 0;
+        // create connection
+        Connection connection = JDBCUtils.getConnection();
+        // create statement
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_USER_LOGIN);
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, user.getPassword());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()) {
+                return resultSet.getInt("type");
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        // execute SQL statements
+        return -1;
     }
 
     @Override
     public boolean insert(User user) {
+        Connection connection = JDBCUtils.getConnection();
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement(SQL_USER_INSERT);
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, Integer.toString(user.getType()));
+            int i = preparedStatement.executeUpdate();
+            if(i > 0) return true;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
         return false;
     }
 
--- src/com/weijiez/test/JDBCUtilsTest.java
+++ src/com/weijiez/test/JDBCUtilsTest.java
@@ -1,6 +1,8 @@
 package com.weijiez.test;
 
+import com.weijiez.dao.UserDao_Imp;
 import com.weijiez.jdbc.JDBCUtils;
+import com.weijiez.user.User;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
 
@@ -22,5 +24,14 @@ public class JDBCUtilsTest {
             System.out.print(resultSet.getString("name") + " ");
             System.out.println(resultSet.getString("password"));
         }
+    }
+
+    @Test
+    public void loginTest() throws Exception {
+        UserDao_Imp userDao_imp = new UserDao_Imp();
+        int type1 = userDao_imp.login(new User("jack", "jack"));
+        int type2 = userDao_imp.login(new User("jack", "jack"));
+        System.out.println(type1);
+        System.out.println(type2);
     }
 }
